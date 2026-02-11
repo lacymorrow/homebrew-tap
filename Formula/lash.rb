f9c8af74b93dfe9973096eee678e8560c11eb1b0
Lacy Morrow <me@lacymorrow.com>
2026-02-11 01:25:03 -0500

    Update to v1.7.7

--- Formula/lash.rb
+++ Formula/lash.rb
@@ -4,13 +4,13 @@
 class Lash < Formula
   desc "The AI coding agent built for the terminal."
   homepage "https://github.com/lacymorrow/lash"
-  version "1.7.6"
+  version "1.7.7"
 
   depends_on "ripgrep"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/lacymorrow/lash/releases/download/v1.7.6/lashcode-darwin-x64.zip"
+      url "https://github.com/lacymorrow/lash/releases/download/v1.7.7/lashcode-darwin-x64.zip"
       sha256 ""
 
       def install
@@ -18,7 +18,7 @@ class Lash < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/lacymorrow/lash/releases/download/v1.7.6/lashcode-darwin-arm64.zip"
+      url "https://github.com/lacymorrow/lash/releases/download/v1.7.7/lashcode-darwin-arm64.zip"
       sha256 ""
 
       def install
@@ -29,14 +29,14 @@ class Lash < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/lacymorrow/lash/releases/download/v1.7.6/lashcode-linux-x64.tar.gz"
+      url "https://github.com/lacymorrow/lash/releases/download/v1.7.7/lashcode-linux-x64.tar.gz"
       sha256 ""
       def install
         bin.install "lash"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/lacymorrow/lash/releases/download/v1.7.6/lashcode-linux-arm64.tar.gz"
+      url "https://github.com/lacymorrow/lash/releases/download/v1.7.7/lashcode-linux-arm64.tar.gz"
       sha256 ""
       def install
         bin.install "lash"
